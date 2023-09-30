a0dcf80f4626d754f239af400590245ec44748fb
shahjalal-bu <mdshahjalal.bu@gmail.com>
2023-09-30 17:03:57 +0600

    add validation

--- src/todo/dto/todo.dto.ts
+++ src/todo/dto/todo.dto.ts
@@ -1,9 +1,16 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsString, IsBoolean } from 'class-validator';
+
+/* eslint-disable prettier/prettier */
 export class CreateTodoDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
 }
 
 export class UpdateTodoDto {
+  @IsString()
   title?: string;
+  @IsBoolean()
   completed?: boolean;
 }
